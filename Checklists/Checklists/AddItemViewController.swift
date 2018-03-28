47af70464d629f23cc9fca20b6d046ca275922db
Fernanda Araujo <nanda.arf@gmail.com>
2018-03-28 15:34:40 -0300

    Reads content of text field and print in console

--- Checklists/Checklists/AddItemViewController.swift
+++ Checklists/Checklists/AddItemViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,6 +23,8 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
+
         navigationController?.popViewController(animated: true)
     }
 }
