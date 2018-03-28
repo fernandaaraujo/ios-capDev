e76a31880ffbfa76bd7de6061c703921ffab6546
Fernanda Araujo <nanda.arf@gmail.com>
2018-03-28 13:13:41 -0300

    Creates AddItemViewController class and assign to add item screen

---
+++ Checklists/Checklists/AddItemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Fernanda Araújo on 28/03/18.
+//  Copyright © 2018 thoughtworks. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
