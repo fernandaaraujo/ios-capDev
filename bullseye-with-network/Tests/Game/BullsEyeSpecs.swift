67f6757bdd8a535ecb319c1eca16bc20ba228b83
Fernanda Araujo <nanda.arf@gmail.com>
2018-04-10 13:19:22 -0300

    Puts target value on before each function

--- bullseye-with-network/Tests/Game/BullsEyeSpecs.swift
+++ bullseye-with-network/Tests/Game/BullsEyeSpecs.swift
@@ -57,9 +57,14 @@ class BullsEyeSpecs: QuickSpec {
       }
       
       describe("score") {
+        var targetValue: Int!
+        
+        beforeEach {
+          targetValue = bullsEye.target
+        }
+        
         it("increments score after hit a value") {
           let hitValue = 50
-          let targetValue = bullsEye.target
           let expectedScore = 100 - abs(targetValue - hitValue)
           
           bullsEye.hit(value: hitValue)
@@ -69,7 +74,6 @@ class BullsEyeSpecs: QuickSpec {
         
         it("sums pontuation to currently score") {
           let hitValue = 50
-          let targetValue = bullsEye.target
           var expectedScore = 100 - abs(targetValue - hitValue)
           
           bullsEye.hit(value: hitValue)
@@ -83,7 +87,6 @@ class BullsEyeSpecs: QuickSpec {
         describe("bonus") {
           it("user hit exact target") {
             let hitValue = bullsEye.target
-            let targetValue = bullsEye.target
             let bonus = 100
             let expectedScore = 100 - abs(targetValue - hitValue) + bonus
             
@@ -94,7 +97,6 @@ class BullsEyeSpecs: QuickSpec {
           
           it("user misses the value by 1 point") {
             let hitValue = bullsEye.target - 1
-            let targetValue = bullsEye.target
             let bonus = 50
             let expectedScore = 100 - abs(targetValue - hitValue) + bonus
             
