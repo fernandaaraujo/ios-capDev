8c5526d117f1ef9724da013cdb41878d561563b7
Fernanda Araujo <nanda.arf@gmail.com>
2018-04-12 17:00:28 -0300

    Refactors GamePresenterSpects

--- bullseye-with-network/Tests/Presenter/GamePresenterSpecs.swift
+++ bullseye-with-network/Tests/Presenter/GamePresenterSpecs.swift
@@ -23,33 +23,24 @@ class GamePresenterSpecs: QuickSpec {
       }
       
       describe("hit") {
+        var gameMock: GameMock!
+        var viewMock: GameViewControllerMock!
+        var presenter: GamePresenter!
+        
+        beforeEach {
+          gameMock = GameMock()
+          viewMock = GameViewControllerMock()
+          presenter = GamePresenter(view: viewMock, game: gameMock)
+        }
+        
         it("calls the Game's hit method with proper value") {
-          let gameMock = GameMock()
-          let viewMock = GameViewControllerMock()
-          let presenter = GamePresenter(view: viewMock, game: gameMock)
-
           presenter.hit(value: 16)
 
           expect(gameMock.didCallHit).to(beTrue())
           expect(gameMock.didCallHitWithValue).to(equal(16))
         }
         
-        it("updates Game's round after hit") {
-          let gameMock = GameMock()
-          let viewMock = GameViewControllerMock()
-          let presenter = GamePresenter(view: viewMock, game: gameMock)
-          let newRound = gameMock.round + 1
-          
-          presenter.hit(value: 16)
-          
-          expect(gameMock.didCallHit).to(beTrue())
-          expect(gameMock.round).to(equal(newRound))
-        }
-        
         it("calls the View to update the score") {
-          let gameMock = GameMock()
-          let viewMock = GameViewControllerMock()
-          let presenter = GamePresenter(view: viewMock, game: gameMock)
           gameMock.score = 666
           
           presenter.hit(value: 16)
@@ -57,11 +48,29 @@ class GamePresenterSpecs: QuickSpec {
           expect(viewMock.didCallUpdateScoreLabel).to(beTrue())
           expect(viewMock.didCallUpdateScoreLabelWithValue).to(equal(666))
         }
+      }
+      
+      describe("newRound") {
+        var gameMock: GameMock!
+        var viewMock: GameViewControllerMock!
+        var presenter: GamePresenter!
+        
+        beforeEach {
+          gameMock = GameMock()
+          viewMock = GameViewControllerMock()
+          presenter = GamePresenter(view: viewMock, game: gameMock)
+        }
+        
+        it("updates Game's round after hit") {
+          let newRound = gameMock.round + 1
+          
+          presenter.hit(value: 16)
+          
+          expect(gameMock.didCallNewRound).to(beTrue())
+          expect(gameMock.round).to(equal(newRound))
+        }
         
         it("call the View to update the round after hit") {
-          let gameMock = GameMock()
-          let viewMock = GameViewControllerMock()
-          let presenter = GamePresenter(view: viewMock, game: gameMock)
           gameMock.round = 1
           
           presenter.hit(value: 16)
@@ -69,6 +78,7 @@ class GamePresenterSpecs: QuickSpec {
           expect(viewMock.didCallUpdateRoundLabel).to(beTrue())
           expect(viewMock.didCallUpdateRoundLabelWithValue).to(equal(2))
         }
+        
       }
     }
   }
@@ -98,15 +108,19 @@ class GameMock: Game {
   
   var didCallHit = false
   var didCallHitWithValue: Int?
+  var didCallNewRound = false
   
   func newRound() {
-    
+    didCallNewRound = true
+  
+    round += round
   }
   
   func hit(value: Int) -> Perfection {
     didCallHit = true
     didCallHitWithValue = value
-    round += round
+    
+    newRound()
     
     return .almost
   }
